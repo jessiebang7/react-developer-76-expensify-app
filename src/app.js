36834f5ad47df54854844d65a990fe1725ecd613
Jessie Bang <jessiebang7@gmail.com>
2019-07-04 13:50:27 -0400

    Add Expensify styles

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ import { startSetExpenses } from './actions/expenses';
 import { login, logout } from './actions/auth';
 
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 const store = configureStore();
 
@@ -50,7 +51,7 @@ const renderApp = () => {
   }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user)=>{
   if(user) {
--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 
 import getVisibleExpenses from '../selectors/expenses';
@@ -9,8 +10,13 @@ export const ExpenseSummary = (props) => {
   const expenseWord = props.expenseCount === 1 ? 'expense' : 'expenses';
   const formattedExpensesTotal = numeral(props.expensesTotal / 100).format('$0,00.00');
   return(
-    <div>
-      <h1>Viewing {props.expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
+    <div className="page-header">
+      <div className="content-container">
+        <h1 className="page-header__title">Viewing <span>{props.expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+        <div className="page-header__actions">
+          <Link className="button" to="/create">Add Expense</Link>
+        </div>
+      </div>
     </div>
   )
 };
